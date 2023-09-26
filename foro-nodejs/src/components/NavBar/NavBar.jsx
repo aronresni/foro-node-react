4f8c00f3f46e0a839b3cc0aced28321df37e8494
aronresni <aronresni@gmail.com>
2023-09-26 18:17:42 -0300

    navbar

--- foro-nodejs/src/components/NavBar/NavBar.jsx
+++ foro-nodejs/src/components/NavBar/NavBar.jsx
@@ -3,7 +3,15 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
     return (
         <div>
-            <></>
+            <div>
+                <img src='logo' />
+            </div>
+            <div>
+                <h1>Searchbar</h1>
+            </div>
+            <div>
+                <h1>User</h1>
+            </div>
         </div>
     )
 }
